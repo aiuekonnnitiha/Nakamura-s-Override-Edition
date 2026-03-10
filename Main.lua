065b97569586b3a769a6f2388177da01d7250a7f
ponitan <168337877+aiuekonnnitiha@users.noreply.github.com>
2026-03-10 14:21:42 +0900

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,14 +1,11 @@
 local _0x1=game:GetService("\80\108\97\121\101\114\115")repeat task.wait() until _0x1.LocalPlayer
 local _0x2=game:GetService("\84\119\101\101\110\83\101\114\118\105\99\101")local _0x3=game:GetService("\76\105\103\104\116\105\110\103")local _0x4=game:GetService("\87\111\114\107\115\112\97\99\101")
-local _0x_FAKEDATA_1 = "はやしかける_PROTECT_V3"
 local _0x5=_0x1.LocalPlayer;local _0x6=_0x5:WaitForChild("\80\108\97\121\101\114\71\117\105")
 local _0x7=Instance.new("\83\99\114\101\101\110\71\117\105")_0x7.IgnoreGuiInset=true;_0x7.Parent=_0x6
 local _0x8=Instance.new("\70\114\97\109\101")_0x8.Size=UDim2.new(1,0,1,0)_0x8.BackgroundColor3=Color3.new(0,0,0)_0x8.BackgroundTransparency=1;_0x8.Parent=_0x7
 local _0x9=Instance.new("\84\101\120\116\76\97\98\101\108")_0x9.Size=UDim2.new(1,0,0.2,0)_0x9.Position=UDim2.new(0,0,0.4,0)_0x9.BackgroundTransparency=1;_0x9.TextColor3=Color3.new(1,1,1)
-local function _0x_GETNAME() local a,b,c,d,e,f = "は","や","し","か","け","る" return a..b..c..d..e..f end
-_0x9.Text=_0x_GETNAME()_0x9.Font=Enum.Font.Code;_0x9.TextSize=35;_0x9.TextTransparency=1;_0x9.Parent=_0x8
+_0x9.Text="軽量化" _0x9.Font=Enum.Font.Code;_0x9.TextSize=35;_0x9.TextTransparency=1;_0x9.Parent=_0x8
 local _0xA=Instance.new("\83\111\117\110\100")_0xA.SoundId="rbxassetid://156353900";_0xA.Volume=0.5;_0xA.Parent=_0x8
-local _0x_FAKEDATA_2 = function() return "はやしかける" end
 task.spawn(function()pcall(function()_0xA:Play()end)_0x2:Create(_0x8,TweenInfo.new(0.3),{BackgroundTransparency=0.4}):Play()_0x2:Create(_0x9,TweenInfo.new(0.5),{TextTransparency=0}):Play()task.wait(1.5)_0x2:Create(_0x9,TweenInfo.new(0.5),{TextTransparency=1}):Play()_0x2:Create(_0x8,TweenInfo.new(0.3),{BackgroundTransparency=1}):Play()task.wait(0.5)_0x7:Destroy()end)
 local function _0xB(_0xC)
 if _0xC:IsA("\80\97\114\116\105\99\108\101\69\109\105\116\116\101\114") or _0xC:IsA("\84\114\97\105\108") or _0xC:IsA("\66\101\97\109") or _0xC:IsA("\69\120\112\108\111\115\105\111\110") or _0xC:IsA("\70\105\114\101") or _0xC:IsA("\83\109\111\107\101") or _0xC:IsA("\83\112\97\114\107\108\101\115")then
